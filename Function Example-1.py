26e62e865673eeb76583ea41fc8577d3d6c44011
karan <sudha142.python@gmail.com>
2021-12-26 20:32:06 +0530

    This is committed message for the My first git project.

---
+++ Function Example-1.py
@@ -0,0 +1,7 @@
+def Name():
+    F_name='sudha'
+    L_name='Rama'
+    Full_name=L_name+F_name
+    return Full_name
+L=Name()
+print('Full name is:',L)
---
+++ Function Example-2.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    c=a+b
+    return c
+l=sum(25,35)
+print("sum of given two numbers are:",l)
